2b469a0a4e6450b8031c5e364d45d4a0d529e994
kaleemahmed2022 <kaleemahmed2022@u.northwestern.edu>
2022-02-27 10:19:42 -0600

    resnet

---
+++ ResNet.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+# from torchvision.transforms import Compose
+
+
+# 3x3 convolution
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
+
+
+# Residual block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    # 3x3 convolution
+    def conv3x3(in_channels, out_channels, stride=1):
+        return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                         stride=stride, padding=1, bias=False)
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+# ResNet
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[1], 2)
+        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+# ResNet 34 example attempt
+class ResNet34(nn.Module):
+    def __init__(self):
+        super(ResNet34,self).__init__()
+
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1,64,kernel_size=2,stride=2,padding=3,bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True)
+        )
+
+        self.block2 = nn.Sequential(
+            nn.MaxPool2d(1,1),
+            ResidualBlock(64,64),
+            ResidualBlock(64,64,2)
+        )
+
+        self.block3 = nn.Sequential(
+            ResidualBlock(64,128),
+            ResidualBlock(128,128,2)
+        )
+
+        self.block4 = nn.Sequential(
+            ResidualBlock(128,256),
+            ResidualBlock(256,256,2)
+        )
+        self.block5 = nn.Sequential(
+            ResidualBlock(256,512),
+            ResidualBlock(512,512,2)
+        )
+
+        self.avgpool = nn.AvgPool2d(2)
+        # vowel_diacritic
+        self.fc1 = nn.Linear(512,11)
+        # grapheme_root
+        self.fc2 = nn.Linear(512,168)
+        # consonant_diacritic
+        self.fc3 = nn.Linear(512,7)
+
+    def forward(self,x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0),-1)
+        x1 = self.fc1(x)
+        x2 = self.fc2(x)
+        x3 = self.fc3(x)
+        return x1,x2,x3
+
+
+
